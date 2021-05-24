6edf5844bc20073e9fd8047fbb83a2ba04a375dc
daniela <daniela.moreno1@mail.udp.cl>
2021-05-24 14:34:23 -0400

    aaaa

--- Javascript/Decrypt_Blowfish_ECB.user.js
+++ Javascript/Decrypt_Blowfish_ECB.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Decrypt_Blowfish_ECB
-// @version      4.4
+// @version      4.5
 // @description  test of interoperability between python and javascript for Blowfish ECB algorithm
 // @author       Daniela Moreno
 // @require      https://gist.githubusercontent.com/aylarov/08eff1bfccb7e0e87a2fef0df766f01c/raw/9c6d8ed3dbb6bafc76fb3c5a6bc5722a41f1f497/ECB_Blowfish.js
@@ -12,7 +12,9 @@
 var key = 'llave'
 var div= document.getElementById("msg_encrypted")
 var msgEncrypted=document.getElementsByClassName("algorithm text-danger")[0].id
+alert(msgEncrypted)
 var result = blowfish.decrypt(msgEncrypted, key, {cipherMode: 0, outputType: 0});
+alert(result)
 div.innerHTML = result
 
 
